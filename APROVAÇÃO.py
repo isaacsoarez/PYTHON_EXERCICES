e2af08cca8798dd4d041749a09b51cd13a93297f
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-08-03 08:39:38 -0300

    Update and rename exercicio03.py to APROVAÇÃO.py

--- APROVAÇÃO.py
+++ APROVAÇÃO.py
@@ -1,13 +1,16 @@
 n1 = float(input('Digite sua primeira nota:'))
 n2 = float(input('Digite sua segunda nota:'))
-media = float(n1+n2)/2
+media = float(n1 + n2) / 2
 
 print('Sua primeira nota é:', n1)
 print('Sua segunda nota é:', n2)
 print('Sua média é:', media)
-if media<=7:
+
+if media < 7:
     print('Você foi reprovado')
-if media>7:
+
+elif media >= 7:
     print('Você foi aprovado')
-else:
+    
+if media >= 9.5:
     print('Você foi aprovado com distinção')
