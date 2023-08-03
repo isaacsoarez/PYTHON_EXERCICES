7fc8aa424fa5d01f7772ec4c6a1d1de60a4c948d
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-08-03 16:34:47 -0300

    Update LUCRO.py

--- LUCRO.py
+++ LUCRO.py
@@ -2,5 +2,5 @@ fat = int(input('Digite seu faturamento:'))
 cus = int(input('Digite seu custo:'))
 luc = fat - cus
 mar = luc / fat
-print('Seu lucro foi de:', luc)
-print('Sua margem foi de:', mar)
+print(f'Seu lucro foi de: R${luc:_.2f}')
+print(f'Sua margem foi de: R$ {mar:_.2f}')
