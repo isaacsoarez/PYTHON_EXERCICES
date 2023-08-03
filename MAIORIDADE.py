6f6cb57f10fe4e5969d4148640b143a835a65bc3
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-08-03 08:22:52 -0300

    Update MAIORIDADE.py

--- MAIORIDADE.py
+++ MAIORIDADE.py
@@ -1,6 +1,6 @@
 Nome= str(input('digite seu nome e sobrenome:'))
 Idade= str(input('digite sua idade:'))
-if Idade<="18":
+if Idade>="18":
     print('você é menor de idade')
 else:
     print('você é maior de idade')
