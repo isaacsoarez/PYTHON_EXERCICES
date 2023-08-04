a007b97950ec9dadc24bc50f747e0315b7840b2f
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-08-04 14:30:46 -0300

    Update APRESENTAÇÃO-NUM-FLOAT-INT.py

--- APRESENTAÇÃO-NUM-FLOAT-INT.py
+++ APRESENTAÇÃO-NUM-FLOAT-INT.py
@@ -1,6 +1,8 @@
 num = 13.744
 print('Números como %s são convertidos para string' % num)
-print('Também podem serem representados como inteiros: %d; repare que sem arredondamento' % num)
+print('Também podem ser representados como inteiros: %d; repare que sem arredondamento' % num)
+print(f'Também podem ser representados como floats: {num:.3f}'.replace('.', ','))
+
 print('Amostra de sinal em inteiros: %+d' % num)
 print('Amostra de sinal em floats: %+f' % num)
-
+print(f'Amostra de sinal em floats (virgula): {num:+.3f}'.replace('.', ','))
