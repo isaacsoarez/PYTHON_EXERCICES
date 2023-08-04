3db55315e49bd936d6a0cab9cde5b025022f4ac1
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-08-04 14:44:25 -0300

    Update APRESENTAÇÃO-NUM-FLOAT-INT.py

--- APRESENTAÇÃO-NUM-FLOAT-INT.py
+++ APRESENTAÇÃO-NUM-FLOAT-INT.py
@@ -6,3 +6,15 @@ print(f'Também podem ser representados como floats: {num:.3f}'.replace('.', ','
 print('Amostra de sinal em inteiros: %+d' % num)
 print('Amostra de sinal em floats: %+f' % num)
 print(f'Amostra de sinal em floats (virgula): {num:+.3f}'.replace('.', ','))
+print('____________________________________________________________')
+num2=179.450
+
+print('Mínimo de 5 caracteres com 2 casas decimais:%5.2f' % num2)
+print('Mínimo de 1 caracteres com 0 casas decimais:%1.0f' % num2)
+print('Mínimo de 1 caracteres com 5 casas decimais:%1.5f' % num2)
+print('Mínimo de 10 caracteres com 2 casas decimais:%10.2f' % num2)
+print('Mínimo de 10 caracteres com 2 casas decimais:%+10.2f (com sinal)' % num2)
+
+
+
+
