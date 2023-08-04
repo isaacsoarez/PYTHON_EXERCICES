ee5f3738b78776c1bb15d5ed986d92e919296841
Isaac Soares <132666435+isaacsoarez@users.noreply.github.com>
2023-08-04 15:15:08 -0300

    Update TROCA-DE-PALAVRAS.py

--- TROCA-DE-PALAVRAS.py
+++ TROCA-DE-PALAVRAS.py
@@ -1,1 +1,7 @@
 print('Um carro %s, outro carro %s e mais um %s' % ('bateu', 'capotou','quebrou'))
+
+print('Um carro {1}, outro carro {0} e mais um {2}'.format('bateu', 'capotou','quebrou'))
+
+print('primeiro:{a}; segundo:{b}; terceiro:{c}'.format(a=1, b=2, c=3))
+
+print(' A variavel{p} pode ser reutilizada:{p}.'.format(p='(oi!)'))
